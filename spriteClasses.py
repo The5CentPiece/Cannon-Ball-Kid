fc7e4d2d184684c4447276b976fe76e6c660db71
The5CentPiece <nickel.the.coin@gmail.com>
2017-08-01 14:58:49 -0500

    Revert "Revert "Revert "bad"""
    This reverts commit 15d03888fce90285c47e105d539878a30ed63a85.

--- spriteClasses.py
+++ spriteClasses.py
@@ -1,28 +1,17 @@
 import pygame
-class Kiddo(pygame.sprite.Sprite):
+class kiddo(pygame.sprite.Sprite):
     def __init__(self,y):
         pygame.sprite.Sprite.__init__(self)
         self.plyrRect = pygame.Rect(910, y+30, 100, 130)
-class Blocks(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.woodRect=[]
-        self.metalRect=[]
-        self.spikeRect=[]
-        self.cannonRect=[]
-class Wood(pygame.sprite.Sprite):
+class block(pygame.sprite.Sprite):
     def __init__(self,r,x,y):
         pygame.sprite.Sprite.__init__(self)
         r.append(pygame.Rect(x, y, 80, 80))
-class Metal(pygame.sprite.Sprite):
-    def __init__(self,r,x,y):
-        pygame.sprite.Sprite.__init__(self)
-        r.append(pygame.Rect(x, y, 80, 80))
-class Spike(pygame.sprite.Sprite):
+class spike(pygame.sprite.Sprite):
     def __init__(self,r,x,y):
         pygame.sprite.Sprite.__init__(self)
         r.append(pygame.Rect(x+20, y, 40, 80))
-class Cannon(pygame.sprite.Sprite):
+class cannon(pygame.sprite.Sprite):
     def __init__(self,r,x,y):
         pygame.sprite.Sprite.__init__(self)
         r.append(pygame.Rect(x, y, 160, 160))
