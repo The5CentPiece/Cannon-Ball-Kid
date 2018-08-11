31b7c70579c45c1558dd22f289a253c95155bd0f
The5CentPiece <john.a.nichols00@gmail.com>
2018-08-11 16:56:21 -0700

    i dont know

--- spriteClasses.py
+++ spriteClasses.py
@@ -2,9 +2,15 @@ import pygame
 class kiddo(pygame.sprite.Sprite):
     def __init__(self,y):
         pygame.sprite.Sprite.__init__(self)
+<<<<<<< HEAD
         self.plyrRect = pygame.Rect(910, y+30, 100, 130)
 class block(pygame.sprite.Sprite):
     def __init__(self,r,x,y):
+=======
+        self.plyrRect = pygame.Rect(910, y+30, 100, 85)
+class wood(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+>>>>>>> parent of e253627... crab image
         pygame.sprite.Sprite.__init__(self)
         r.append(pygame.Rect(x, y, 80, 80))
 class spike(pygame.sprite.Sprite):
